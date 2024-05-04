f6cd315fe13a7bbbe4b56723ca064bc6173d5f11
nithish6470 <prk.nithish55@gmail.com>
2024-05-04 23:21:31 +0530

    update

---
+++ Wetherdetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Wetherdetails() {
+  return (
+    <div className='text-white border-[1px] lg:w-1/3 w-1/2 h-32 mt-10 rounded-lg'>
+      dfsf
+    </div>
+  )
+}
+
+export default Wetherdetails
